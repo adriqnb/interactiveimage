7b0d0137268d0fe8b8bc1612119e1a07135778e1
apervelis <alexperevelis@gmail.com>
2025-11-10 11:40:26 -0500

    stretchy top triangle

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 let img;
+let pullBack = 1;
+let mouseXorig;
+let color;
+let mouseYorig;
 function preload()
 {
 img = loadImage('assets/interactive.jpg');
@@ -13,20 +17,22 @@ function draw() {
   
   image(img, 0, 0);
   middleSquare();
-
   // display mouse coordinates
   fill(255);
   noStroke();
   rect(0, height - 20, 100, 20);
   fill(0);
   text("X: " + mouseX + " Y: " + mouseY, 10, height - 5);
-  fill('black');
+  fill('black');//black triangle
   triangle(29,323,172,179,168,464);
-  fill(162,32,6);
-  triangle(193,160,332,20,470,161);
-  fill(231,219,119);
+  
+  
+  
+  
+  fill(231,219,119);//yellow triangle
   triangle(294,597,368,598,331,636);
-  fill(223,229,225);
+
+  fill(223,229,225);//white triangle
   triangle(571,264,574,387,635,325);
 
   // highlight triangles on mouseover
@@ -51,16 +57,14 @@ function draw() {
   if (mouseX > 332 && mouseX < 645 && mouseY > 325 && mouseY < 645 && mouseY > (315 / 313) * (mouseX - 645) + 645) {
     triangle(645, 645, 645, 332, 332, 645);
   }
+  // ---------red triangle----------
+  fill(162,32,6);
+  if(pullBack === 0 && mouseX >= mouseXorig)
+    triangle((193-((mouseX-mouseXorig)/5)),160,(332),20,470+((mouseX-mouseXorig)/5),161);
+  else
+    triangle(193,160,332,20,470,161);
+  //---------------------------------
 }
-
-
-
-
-
-
-
-
-
 // top left triangle: M(20, 10) R(332, 10) L(20, 325)
 // top right triangle M(645, 10) R(645, 325) L(332, 10)
 // bottom left triangle M(20, 645) R(332, 645) L(20, 325)
@@ -74,5 +78,10 @@ function middleSquare(){
   fill(x,0,y,150);
   rect(170,179,272,285);
 }
-
-
+function mousePressed(){
+    pullBack = 0;
+    mouseXorig = mouseX;
+  }
+function mouseReleased(){
+  pullBack = 1;
+}
