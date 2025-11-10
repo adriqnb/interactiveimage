75245c836c53d213fc8726be69ff32e98aa354a7
lmelfa <luke.melfa@stonybrook.edu>
2025-11-10 10:33:01 -0500

    Simon says added

--- sketch.js
+++ sketch.js
@@ -20,6 +20,29 @@ function draw() {
   rect(0, height - 20, 100, 20);
   fill(0);
   text("X: " + mouseX + " Y: " + mouseY, 10, height - 5);
+
+  // highlight triangles on mouseover
+  noStroke();
+  fill(255, 0, 0, 100);
+  // top left triangle
+  if (mouseX > 20 && mouseX < 332 && mouseY > 10 && mouseY < 325 && mouseY < (mouseX - 20) * (315 / 312) + 10) {
+    triangle(20, 10, 332, 10, 20, 325);
+  }
+  // top right triangle
+  fill(0, 0, 255, 100);
+  if (mouseX > 332 && mouseX < 645 && mouseY > 10 && mouseY < 325 && mouseY < (-315 / 313) * (mouseX - 645) + 10) {
+    triangle(645, 10, 645, 325, 332, 10);
+  }
+  // bottom left triangle
+  fill(0, 255, 0, 100);
+  if (mouseX > 20 && mouseX < 332 && mouseY > 325 && mouseY < 645 && mouseY > (-315 / 312) * (mouseX - 20) + 645) {
+    triangle(20, 645, 332, 645, 20, 325);
+  }
+  // bottom right triangle
+  fill(255, 255, 0, 100);
+  if (mouseX > 332 && mouseX < 645 && mouseY > 325 && mouseY < 645 && mouseY > (315 / 313) * (mouseX - 645) + 645) {
+    triangle(645, 645, 645, 332, 332, 645);
+  }
 }
 
 // top left triangle: M(20, 10) R(332, 10) L(20, 325)
